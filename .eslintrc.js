567a0f8095de43f30dbeb55c3648b097569d8fd3
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2019-02-22 09:24:43 +0100

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   root: true,
   env: {
+    es6: true,
+    browser: true,
     node: true
   },
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    semi: [0, 'always'],
+    'max-len': ["warning", { "code": 100, "ignoreComments": true }]
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
