32f3ebe91b20309fef67a125323463e2c2b965e4
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2019-02-23 12:39:19 +0100

    Configure i18n

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueI18n from "vue-i18n";
+Vue.use(VueI18n);
+
+import { languages } from "@/i18n";
+import { defaultLocale } from "@/i18n";
+const messages = Object.assign(languages);
 
 Vue.config.productionTip = false;
 
+var i18n = new VueI18n({
+  locale: defaultLocale,
+  fallbackLocale: "de",
+  messages
+});
+
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
 
